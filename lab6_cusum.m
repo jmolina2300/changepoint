61629185335f8530ef28ccc9f4d002a248b8fe38
Jan Molina <jm8721@engr.ship.edu>
2023-11-02 09:34:43 -0400

    Add different scales to axes

--- lab6_cusum.m
+++ lab6_cusum.m
@@ -15,16 +15,23 @@
 global global_points;
 
 desired_confidence = 95;  %input('Enter confidence level: ');
-%data = readmatrix("TwoMeans.csv");
-data = readmatrix("changes2.csv");
+data = readmatrix("TwoMeans.csv");
 data = data(2:end,1);
 
-figure(2)
-plot(data,'LineWidth',1)
 
 
 global_points = [];
 find_changepoints(data,desired_confidence, 0);
+
+% Set scale of axes 
+yyaxis left
+xlim([0 length(data)])
+figure(1);
+plot(data,'LineWidth',0.5)
+
+
+
+
 
 
 
